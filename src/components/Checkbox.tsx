52764ef6b74b6e63f898a4161365eec33cb6b625
zwiro <josiu100@gmail.com>
2023-03-01 22:06:33 +0100

    Checkbox component

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -5,17 +5,22 @@ type CheckboxProps = {
 
 function Checkbox({ value, isCategory }: CheckboxProps) {
   return (
-    <>
+    <div className="flex items-center gap-2">
       <input
         type="checkbox"
         id={value}
         value={value}
-        className="accent-zinc-700"
+        className="h-3 w-3 cursor-pointer appearance-none border border-zinc-700 bg-transparent transition-colors checked:bg-zinc-700"
       />
-      <label htmlFor={value} className={`${isCategory && "text-xl font-bold"}`}>
+      <label
+        htmlFor={value}
+        className={`cursor-pointer transition-colors hover:text-blue-600 ${
+          isCategory && "text-xl font-bold"
+        }`}
+      >
         {value}
       </label>
-    </>
+    </div>
   )
 }
 
--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -4,22 +4,21 @@ import { categories } from "./Nav"
 function Filters() {
   return (
     <div className="mt-4">
-      {categories.map((category, i) => (
-        <div key={`${category}-${i}`}>
-          <div className="flex gap-2">
-            <Checkbox value={category.name} isCategory />
-          </div>
-          <ul>
-            {category.subcategories?.map((subcategory, i) => (
-              <li key={`${subcategory}-${i}`}>
-                <div className="flex items-center gap-2">
-                  <Checkbox value={subcategory} />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      {categories.map(
+        (category, i) =>
+          category.name !== "All items" && (
+            <div key={`${category}-${i}`}>
+              <Checkbox value={category.name} isCategory />
+              <ul>
+                {category.subcategories?.map((subcategory, i) => (
+                  <li key={`${subcategory}-${i}`}>
+                    <Checkbox value={subcategory} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )
+      )}
     </div>
   )
 }
